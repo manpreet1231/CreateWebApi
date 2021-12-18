47797138cac911e26906a29b328424b56a583c0f
azharabbas787 <96197943+manpreet1231@users.noreply.github.com>
2021-12-18 22:13:46 +0500

    CreateWebApi
    Calculator  Create Web Api

---
+++ CreateWebApi/CreateWebApi/Calculator.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CreateWebApi
+{
+    public class Calculator
+    {
+        public decimal Addition { get; set; }
+        public decimal Subtraction { get; set; }
+        public decimal Multiplication { get; set; }
+        public decimal Division { get; set; }
+    }
+}
---
+++ CreateWebApi/CreateWebApi/Controllers/CalculatorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [FromQuery]
+        public decimal input1 { get; set; }
+        [FromQuery]
+        public decimal input2 { get; set; }
+        [HttpGet]
+        public IEnumerable<Calculator> Get([FromQuery] decimal input1, [FromQuery] decimal input2)
+        {
+            return Enumerable.Range(1, 1).Select(index => new Calculator
+            {
+                Addition = input1 + input2,
+                Subtraction = input1 - input2,
+                Multiplication = input1 * input2,
+                Division = input1 / input2
+            })
+            .ToArray();
+        }
+    }
+}
